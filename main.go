50dacdc55feae2b1cf05dfac6b52d180a72a86d3
Chris Glass <tribaal@gmail.com>
2014-06-17 10:14:44 +0200

    Added comment

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "bpickle"
 import "fmt"
 
+// This is just for mucking about while testing/developping, it's not meant for
+// anything serious.
 func main() {
 	var result = bpickle.Dumps(666)
 	fmt.Println(result)
