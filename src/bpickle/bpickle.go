2d77344c5dcbcf4bfb666f5c0249bd97d6c87084
Chris Glass <tribaal@gmail.com>
2014-06-17 10:02:40 +0200

    Added type-switching logic and string handling
    Hurray for default utf8/unicode :)

--- src/bpickle/bpickle.go
+++ src/bpickle/bpickle.go
@@ -2,8 +2,26 @@ package bpickle
 
 import "fmt"
 
-func Test() {
-    fmt.Println(dumpBool(false))
+
+type Anything interface{}
+
+// The main function to create Bpickle strings. You can pass "anything" to it
+// and it will take care of converting it to whatever is relevant.
+func Dumps(anything Anything) string {
+    var result string
+    switch anything.(type){
+        case bool:
+            result = dumpBool(anything.(bool))
+        case float32:
+            result = dumpFloat32(anything.(float32))
+        case float64:
+            result = dumpFloat64(anything.(float64))
+        case int:
+            result = dumpInt(anything.(int))
+        case string:
+            result = dumpString(anything.(string))
+    }
+    return result
 }
 
 func dumpBool(object bool) string {
@@ -20,7 +38,17 @@ func dumpInt(object int) string {
     return result
 }
 
-func dumpFloat(object float32) string {
-    var result string = fmt.Sprintf("i%f", object)
+func dumpFloat32(object float32) string {
+    var result string = fmt.Sprintf("i%r;", object)
+    return result
+}
+
+func dumpFloat64(object float64) string {
+    var result string = fmt.Sprintf("i%r;", object)
+    return result
+}
+
+func dumpString(object string) string {
+    var result string = fmt.Sprintf("u:%d:%s", len(object), object)
     return result
 }
--- src/bpickle/bpickle_test.go
+++ src/bpickle/bpickle_test.go
@@ -5,29 +5,54 @@ import (
 )
 
 func Test_boolean_true(t *testing.T) {
-    var result string = dumpBool(true)
+    var result string = Dumps(true)
     if result != "b1" {
         t.Fail()
     }
 }
 
 func Test_boolean_false(t *testing.T) {
-    var result string = dumpBool(false)
+    var result string = Dumps(false)
     if result != "b0" {
         t.Fail()
     }
 }
 
 func Test_integer_positive(t *testing.T) {
-    var result string = dumpInt(100)
+    var result string = Dumps(100)
     if result != "i100;" {
         t.Fail()
     }
 }
 
 func Test_integer_negative(t *testing.T) {
-    var result string = dumpInt(-100)
+    var result string = Dumps(-100)
     if result != "i-100;" {
         t.Fail()
+    }
+}
+
+func Test_string(t *testing.T){
+    var result string = Dumps("some string")
+    if result != "u:11:some string" {
+        t.Error(result)
+    }
+}
+
+//func Test_float32_positive(t *testing.T) {
+func float32_positive(t *testing.T) {
+    var input float32 = 123.45
+    var result string = dumpFloat32(input)
+    if result != "f123.45;" {
+        t.Error(result)
+    }
+}
+
+//func Test_float32_negative(t *testing.T) {
+func float32_negative(t *testing.T) {
+    var input float32 = -123.45
+    var result string = dumpFloat32(input)
+    if result != "f-123.45;" {
+        t.Error(result)
     }
 }
